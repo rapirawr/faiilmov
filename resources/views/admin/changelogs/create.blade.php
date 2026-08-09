95184a5630f884aeab8d6f6b1b2d28a957710f1f
rapirawr <animekayangg313@gmail.com>
2026-08-09 12:29:47 +0700

    fix_seo

--- resources/views/admin/changelogs/create.blade.php
+++ resources/views/admin/changelogs/create.blade.php
@@ -34,7 +34,7 @@
                 <!-- Version -->
                 <div>
                     <label class="block text-xs font-semibold text-zinc-300 uppercase tracking-wider mb-2">Nomor Versi *</label>
-                    <input type="text" name="version" value="{{ old('version', 'v2.5.0') }}" required placeholder="Contoh: v2.5.0" 
+                    <input type="text" name="version"  required placeholder="Contoh: v2.5.0" 
                            class="w-full bg-zinc-950 border border-white/10 rounded-xl px-4 py-2.5 text-sm text-white font-mono focus:outline-none focus:border-amber-500">
                 </div>
 
