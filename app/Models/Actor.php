f2b5647beae6dd21a2e70cef7c691bb9b21cb1e6
rapirawr <animekayangg313@gmail.com>
2026-08-09 19:25:22 +0700

    feat: default actor photo to gray person avatar placeholder in Model accessor and Blade views

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -15,4 +15,17 @@ class Actor extends Model
     {
         return $this->belongsToMany(Film::class, 'film_actor')->withPivot('character_name');
     }
+
+    /**
+     * Get gray person avatar SVG data URI if photo_url is empty
+     */
+    public function getPhotoUrlAttribute($value): ?string
+    {
+        if (!empty($value) && !str_contains($value, 'unsplash.com')) {
+            return $value;
+        }
+
+        // Return a clean gray person avatar SVG Data URI placeholder
+        return 'data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24" fill="%239ca3af"><rect width="100%" height="100%" fill="%2327272a"/><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>';
+    }
 }
